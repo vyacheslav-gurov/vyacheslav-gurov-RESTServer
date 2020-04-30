f551f41cdb818c91c3c98e43cf08525a430a1be2
Гуров Вячеслав <v.gurov@ftc.ru>
2020-04-30 17:21:01 +0700

    Create empty project on start.spring.io

---
+++ src/main/java/org/organization/restapi/RestapiApplication.java
@@ -0,0 +1,13 @@
+package org.organization.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiApplication.class, args);
+	}
+
+}
